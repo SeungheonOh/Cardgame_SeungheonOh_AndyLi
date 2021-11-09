5dbcca8b4af68d7f9de940ca91f592af2982bfae
landy8697 <landy8697@gmail.com>
2021-11-09 07:33:37 -0600

    api changes

--- api.py
+++ api.py
@@ -20,27 +20,32 @@ drawParser    = lambda d: d
 
 apiDef = { 
   "new"    : ("/deck/new/shuffle/?deck_count={}", wrapFailure(newDeckParser))
-, "throw":   ("/deck/{}/pile/{}/draw/>cards={}", wrapFailure(id))
-, "add": ("/deck/{}/pile/{}/add/cards={}", wrapFailure(id))
+, "throw":   ("/deck/{}/pile/{}/draw/>?cards={}", wrapFailure(id))
+, "add": ("/deck/{}/pile/{}/add/?cards={}", wrapFailure(id))
 , "random" : ("/deck/{}/draw/?count={}", wrapFailure(drawParser))
 , "shuffle": ("/deck/{}/shuffle/?remainting={}", wrapFailure(id))
+, "list":  ("/deck/{}/pile/{}/list", wrapFailure(id))
 }
 
 def get_code_list(dicta): 
-  list = []
+  list1 = []
   for card in dicta['cards']:
-    list.append(card['code'])
-  print(list)
-  return list
+    list1.append(card['code'])
+  str1 = ""
+  for card in list1:
+    str1+=card+","
+  str1 = str1[0:len(str1)-1]
+  return str1
 
 api = mkEndpoints(apiDef)
 
-deck_id = api["new"](4)
+deck_id = api["new"](1)
 print(deck_id)
 
 dicta = api["random"](deck_id, 5)
 print(dicta)
 print(api["add"](deck_id, "pileid", get_code_list(dicta)))
+print(api["list"](deck_id, "pileid"))
 #print(api["add"](deck_id))
 
 '''
